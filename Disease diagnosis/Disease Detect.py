f1fe821733f3036f92ecaa1e193b74de4db702ce
Eden <xiang_xuan@qq.com>
2022-02-24 20:28:24 +0800

    :sparkles: feat: Logistic Regression for disease detect

---
+++ Disease diagnosis/Disease Detect.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
+
+data = pd.read_csv('../data/Heart Disease Dataset.csv')
+
+# 对不连续特征进行onehot编码
+data = pd.get_dummies(data, columns=['sex'])
+
+y_data = data['target']
+x_data = data.drop(columns=['target'])
+
+
+# 标准化
+standardizer = StandardScaler()
+standardizer.fit(x_data)
+x_data = standardizer.transform(x_data)
+
+x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.3)
+
+classifier = LogisticRegression()
+classifier.fit(x_train, y_train)
+
+report = classification_report(y_test, classifier.predict(x_test))
+print(report)

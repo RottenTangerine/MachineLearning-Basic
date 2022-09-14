:bug: fix: Fix optimizer gradient vanish problem

Practice/CIFAR-100 classification/VGG.py
@@ -1,27 +1,23 @@
-
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import torchvision
 from torchvision import datasets, transforms
 
-import matplotlib.pyplot as plt
-
 train_dataset = torchvision.datasets.CIFAR100(root='../../data', train=True, download=True, transform=transforms.ToTensor())
 test_dataset = torchvision.datasets.CIFAR100(root='../../data', train=False, download=True, transform=transforms.ToTensor())
-
 
 # hyper params
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # hyper parameters
-epoch_num = 50
-batch_size = 32
-learning_rate = 1e-2
+epoch_num = 20
+batch_size = 16
+learning_rate = 0.01
 class_num = 100
 
 train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
 test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=True)
+
 
 class VGG(nn.Module):
     def __init__(self, class_num=100):
@@ -39,17 +35,30 @@ class VGG(nn.Module):
                 in_channels = v
 
         self.features = nn.Sequential(*layers)
-        self.avgPool = nn.AdaptiveAvgPool2d(7)
+        self.avgPool = nn.AdaptiveAvgPool2d(1)
         self.classifier = nn.Sequential(
-            nn.Linear(512 * 7 * 7, 4096),
+            nn.Linear(512 * 1 * 1, 4096),
             nn.ReLU(True),
             nn.Dropout(),
             nn.Linear(4096, 4096),
             nn.ReLU(True),
             nn.Dropout(),
-            nn.Linear(4096, class_num)
+            nn.Linear(4096, class_num),
         )
 
+        # model init
+        for m in self.modules():
+            match m:
+                case nn.Conv2d:
+                    nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                    if m.bias is not None:
+                        nn.init.constant_(m.bias, 0)
+                case nn.BatchNorm2d:
+                    nn.init.constant_(m.weight, 1)
+                    nn.init.constant_(m.bias, 1)
+                case nn.Linear:
+                    nn.init.normal_(m.weight, 0, 0.01)
+                    nn.init.constant_(m.bias, 0)
 
     def forward(self, x):
         x = self.features(x)
@@ -61,15 +70,14 @@ class VGG(nn.Module):
 
 model = VGG().to(device)
 criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=5e-4)
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
-torch.cuda.empty_cache()
+
 
 for epoch in range(epoch_num):
     for i, (pic, label) in enumerate(train_loader):
         pic = pic.to(device)
         label = label.to(device)
-
         output = model(pic)
         loss = criterion(output, label)
 
@@ -77,8 +85,9 @@ for epoch in range(epoch_num):
         loss.backward()
         optimizer.step()
 
-        if (i + 1) % 28 == 0:
-            print(f'Epoch: {epoch},batch: {i + 1}, loss: {loss.item():.4f}, lr:{optimizer.param_groups[0]["lr"]:.6f}')
+        if i % 500 == 0:
+            print(f'Training Epoch: {epoch + 1}/{epoch_num} [{i}/{len(train_loader)}]\tLoss: {loss.item():0.4f}\tLR: {learning_rate:0.6f}')
+
 
 with torch.no_grad():
     correct = 0
@@ -95,3 +104,4 @@ with torch.no_grad():
 
     print(f'Accruacy: {correct / total}')
 
+torch.save(model.state_dict(), 'model.pth')
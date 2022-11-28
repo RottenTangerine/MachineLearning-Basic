04ab0ec1edf6851e1af1f5ecc68f3302e60b5741
Eden Xiang <xiang_xuan@qq.com>
2022-11-29 02:27:30 +0800

    :art: perf(emotion detection): pref the performance

--- Practice/Emotion Detection/camera.py
+++ Practice/Emotion Detection/camera.py
@@ -1,55 +1,16 @@
 import cv2
 import numpy as np
 import torch
-import torch.nn as nn
 import matplotlib.pyplot as plt
-
-class CNN(nn.Module):
-    def __init__(self,):
-        super(CNN, self).__init__()
-
-        self.layer1 = nn.Sequential(
-            nn.Conv2d(1, 16, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(16),
-            nn.ReLU(),
-        )
-
-        self.layer2 = nn.Sequential(
-            nn.Conv2d(16, 128, kernel_size=5, stride=1, padding=2),
-            nn.BatchNorm2d(128),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2)
-        )
-
-        self.layer3 = nn.Sequential(
-            nn.Conv2d(128, 512, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(512),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2)
-        )
-
-        self.layer4 = nn.Sequential(
-            nn.Conv2d(512, 16, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(16),
-            nn.ReLU()
-        )
-
-        self.linear = nn.Linear(12 * 12 * 16, class_num)
-
-    def forward(self,x):
-        out = self.layer1(x)
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = self.layer4(out)
-
-        out = out.reshape(out.size(0), -1)
-        return self.linear(out)
+import matplotlib.patches as patches
 
 idx_to_class = ['angry', 'disgusted', 'fearful', 'happy', 'neutral', 'sad', 'surprised']
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-model = torch.load('model.ckpt').to(device)
+model = torch.load('model.pth').to(device)
+
 
 def predict_pic(pic, model, device, idx_to_class):
+    pic = pic[200:400, 220:420]
     pic = cv2.resize(pic, (48, 48))
     pic = np.reshape(pic, (1, 1, 48, 48))
     tensor = torch.from_numpy(pic).to(device).float()
@@ -58,6 +19,7 @@ def predict_pic(pic, model, device, idx_to_class):
     _, predicted = torch.max(output, 1)
 
     return idx_to_class[predicted]
+
 
 camera = cv2.VideoCapture(0)
 if not camera.isOpened():
@@ -72,9 +34,14 @@ while True:
         print("Can't receive frame (stream end?). Exiting ...")
         break
 
-    if frame_counter%60 == 0:
+    if frame_counter % 60 == 0:
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
         plt.imshow(frame)
+        currentAxis = plt.gca()
+        rect = patches.Rectangle((220, 200), 200, 200, linewidth=3, edgecolor='r', facecolor='none')
+        currentAxis.add_patch(rect)
+
         frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
         cv2.resize(frame, (48, 48))
         output = predict_pic(frame, model, device, idx_to_class)
